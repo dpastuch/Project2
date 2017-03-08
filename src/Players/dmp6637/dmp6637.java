4f3b8ce56514b9fc379a664a6c38dd09394210c6
dpastuch <d1pastuch@rochester.rr.com>
2017-03-08 11:21:16 -0500

    Created project structure. Created and stubbed out player module class (dmp6637.java).

---
+++ src/Players/dmp6637/dmp6637.java
@@ -0,0 +1,35 @@
+package Players.dmp6637;
+
+import Interface.PlayerModulePart1;
+import Interface.PlayerMove;
+
+/**
+ * Created by d1pas on 3/8/2017.
+ */
+public class dmp6637 implements PlayerModulePart1 {
+
+    @Override
+    public void initPlayer(int dim, int playerId) {
+
+    }
+
+    @Override
+    public void lastMove(PlayerMove m) {
+
+    }
+
+    @Override
+    public PlayerMove move() {
+        return null;
+    }
+
+    @Override
+    public void otherPlayerInvalidated() {
+
+    }
+
+    @Override
+    public boolean hasWonGame(int id) {
+        return false;
+    }
+}
