6fda303e22933a92444efb6a22e70d8d81804754
dpastuch <d1pastuch@rochester.rr.com>
2017-03-22 21:23:19 -0400

    Continued work on BFS; conducted some testing. Needs more work to complete all tests.

--- src/Players/dmp6637/Node.java
+++ src/Players/dmp6637/Node.java
@@ -1,32 +1,70 @@
 package Players.dmp6637;
 
+import Interface.Coordinate;
+
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Players.dmp6637.Node for use in a pathbuilder game.
+ * Node for use in a pathbuilder game.
  *
  * @author dmp6637 (David Pastuch)
  */
-public class Node {
+public class Node{
 
     private int playerId;
-    private int row;
-    private int col;
     private Set<Node> neighbors;
+    private Coordinate coord;
 
+    /**
+     * Create a new node.
+     * @param playerId player Id
+     * @param row row
+     * @param col column
+     */
     public Node(int playerId, int row, int col) {
         this.playerId = playerId;
-        this.row = row;
-        this.col = col;
-        neighbors = new HashSet<>();
+        neighbors = new HashSet<Node>();
+        coord = new Coordinate(row, col);
     }
 
+    /**
+     * The the ID of the player this node belongs to.
+     * @return player ID
+     */
     public int getId() {
         return playerId;
     }
 
+    /**
+     * Add a neighbor to this node's set of neighbors.
+     * @param n node to add
+     */
     public void addNeighbor(Node n) {
         neighbors.add(n);
+    }
+
+    /**
+     * Get this node's set of neighbors.
+     * @return neighbors
+     */
+    public Set<Node> getNeighbors() {
+        return neighbors;
+    }
+
+    /**
+     * Get this node's coordinates
+     * @return
+     */
+    public Coordinate getCoordinate() {
+        return coord;
+    }
+
+    public void setId(int Id) {
+        playerId = Id;
+    }
+
+    public String toString() {
+        return Integer.toString(playerId);
     }
 }
--- src/Players/dmp6637/dmp6637.java
+++ src/Players/dmp6637/dmp6637.java
@@ -3,8 +3,13 @@ package Players.dmp6637;
 import Interface.PlayerModulePart1;
 import Interface.PlayerMove;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 /**
  * A class that works as a player module.
+ *
+ * @author dmp6637 (David Pastuch)
  */
 public class dmp6637 implements PlayerModulePart1 {
 
@@ -18,8 +23,26 @@ public class dmp6637 implements PlayerModulePart1 {
      */
     @Override
     public void initPlayer(int dim, int playerId) {
-        board = new Node[dim*2][dim*2];
+        board = new Node[dim*2 + 1][dim*2 + 1];
         this.playerId = playerId;
+        for(int i = 0; i <= dim*2; i++) {
+            if(i == 0) {
+                for(int j = 0; j <= dim*2; j++) {
+                    board[i][j] = new Node(1, i, j);
+                }
+            }
+            for(int j = 0; j <= dim*2; j++) {
+                if(j == 0) {
+                    board[i][j] = new Node(2, i, j);
+                }
+                else if(((i % 2) == 0) && ((j % 2) == 1)) {
+                    board[i][j] = new Node(1, i, j);
+                }
+                else if(((i % 2) == 1) && ((j % 2) == 0)) {
+                    board[i][j] = new Node(2, i, j);
+                }
+            }
+        }
     }
 
     /**
@@ -31,8 +54,8 @@ public class dmp6637 implements PlayerModulePart1 {
     public void lastMove(PlayerMove m) {
         int row = m.getCoordinate().getRow();
         int col = m.getCoordinate().getCol();
-        Node n = new Node(playerId, row, col);
-        board[row][col] = n;
+        Node n = new Node(m.getPlayerId(), row, col);
+        board[col][row] = n;
         setNeighbor(n, row - 1, col);
         setNeighbor(n, row + 1, col);
         setNeighbor(n, row, col - 1);
@@ -65,14 +88,65 @@ public class dmp6637 implements PlayerModulePart1 {
     @Override
     public boolean hasWonGame(int id) {
         //TODO breadth first search
+        if(id == 1) {
+            winningPath(0,1,id, board.length - 1);
+        }
+        if(id == 2) {
+            winningPath(1,0,id, board.length - 1);
+        }
         return false;
     }
 
+    /**
+     * Helper method that adds a node to another node's list of neighbors.
+     * @param n node to add neighbor to
+     * @param row row of node to add
+     * @param col column of node to add
+     */
     private void setNeighbor(Node n, int row, int col) {
-        if(row > 0 && row < board.length && col > 0 && col < board.length) {
-            if(board[row][col] != null) {
-                n.addNeighbor(board[row][col]);
+        if(row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            if(board[col][row] != null && board[col][row].getId() == playerId) {
+                n.addNeighbor(board[col][row]);
+                board[col][row].addNeighbor(n);
             }
         }
+    }
+
+    /**
+     * Check if there is a winning path from this node.
+     * @param x x coordinate of node to check
+     * @param y y coordinate of node to check
+     * @param playerId player Id
+     * @param dim size of board
+     * @return true if there is a winning path, otherwise false
+     */
+    private boolean winningPath(int x, int y, int playerId, int dim) {
+        HashSet<Node> s = new HashSet<>();
+        LinkedList<Node> l = new LinkedList<>();
+
+        l.addLast(board[y][x]);
+        s.add(board[y][x]);
+
+        while(!(l.isEmpty())) {
+            Node curr = l.pop();
+            if(playerId == 1) {
+                if(curr.getCoordinate().getCol() == dim) {
+                    return true;
+                }
+            }
+            else if(playerId == 2) {
+                if(curr.getCoordinate().getRow() == dim) {
+                    return true;
+                }
+            }
+            for(Node n : curr.getNeighbors()) {
+                if(!(s.contains(n))) {
+                    l.addLast(n);
+                    s.add(n);
+                }
+
+            }
+        }
+        return false;
     }
 }
